0ab91b31a71e8c7249b76ba802eef98ace140adc
arundevks <arundevks@gmail.com>
2015-05-29 10:43:43 +0530

    Initial commit

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Arundev K S on 29/05/15.
+//  Copyright (c) 2015 QBurst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Arundev K S on 29/05/15.
+//  Copyright (c) 2015 QBurst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController {
+    
+    IBOutlet UITextField *textField;
+    IBOutlet UILabel *label;
+}
+
+
+@end
+
